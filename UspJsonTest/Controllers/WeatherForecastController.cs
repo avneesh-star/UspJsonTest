b528680e3dbcd78c911294dd2721e53974748157
avneesh <avneeshgngwr@gmail.com>
2022-02-24 00:01:26 +0530

    Add project files.

---
+++ UspJsonTest/Controllers/WeatherForecastController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UspJsonTest.Data;
+using UspJsonTest.Models;
+
+namespace UspJsonTest.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly AppDbContext _context;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, AppDbContext appDbcontext)
+        {
+            _logger = logger;
+            _context = appDbcontext;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet("cust/{id}")]
+        public  async Task<IActionResult> Getcust(int id = 0)
+        {
+            var data = await _context.Procedures.sp_get_stateAsync(0);
+            return Ok(data);
+        }
+    }
+}
